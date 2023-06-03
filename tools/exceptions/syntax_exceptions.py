2eca13c6bb328a980956973342577094c141cbc3
Tim <BerkyT115@yandex.ru>
2023-06-03 17:16:32 +0300

    Добавил функциональности в классы исключений

--- tools/exceptions/syntax_exceptions.py
+++ tools/exceptions/syntax_exceptions.py
@@ -2,4 +2,5 @@ from tools.exceptions.main_exception import MainException
 
 
 class SyntaxException(MainException):
-    pass
+    def __init__(self, *args):
+        super().__init__(*args, default='Syntax error!')
